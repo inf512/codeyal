03c31e26556624bc7bf7df3194ba4919a14b0623
Limbert <limbertx@hotmail.com>
2017-11-17 11:22:58 -0400

    adicion de archivo google search meta

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-  <title>My app heroku and github</title>
+  	<title>My app heroku and github</title>
+	<meta name="google-site-verification" content="9F8vmckXEDRla9N9m0EKu6Vk6jVoGeE_vcKd7JXfIsI" />
 </head>
 <body>
   <h1>heroku y github online</h1>
