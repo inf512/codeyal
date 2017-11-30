2d1fc36d5ead62eb836275379171fb1846c075ca
Limbert <limbertx@hotmail.com>
2017-11-30 16:39:30 -0400

    actualizacion de imagenes

--- galeria.php
+++ galeria.php
@@ -19,11 +19,11 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/gm-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Creacion de </h4>
-          <p class="text-muted">Illustration</p>
+          <h4>Planos</h4>
+          <p class="text-muted">Vizualizador de planos</p>
         </div>
       </div>
       <div class="col-md-4 col-sm-6 portfolio-item">
@@ -33,11 +33,11 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/02-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/act_equipo-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Explore</h4>
-          <p class="text-muted">Graphic Design</p>
+          <h4>Equipo</h4>
+          <p class="text-muted">Gestion de equipos</p>
         </div>
       </div>
       <div class="col-md-4 col-sm-6 portfolio-item">
@@ -47,11 +47,11 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/03-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/act_productiva-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Finish</h4>
-          <p class="text-muted">Identity</p>
+          <h4>Actividad Productiva</h4>
+          <p class="text-muted">Gestion de actividades productivas</p>
         </div>
       </div>
       <div class="col-md-4 col-sm-6 portfolio-item">
@@ -61,11 +61,11 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/04-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/act_productiva-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Lines</h4>
-          <p class="text-muted">Branding</p>
+          <h4>Personal</h4>
+          <p class="text-muted">Gestion de personal</p>
         </div>
       </div>
       <div class="col-md-4 col-sm-6 portfolio-item">
@@ -75,11 +75,11 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/05-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/act_productiva-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Southwest</h4>
-          <p class="text-muted">Website Design</p>
+          <h4>Personal</h4>
+          <p class="text-muted">Gestion de personal</p>
         </div>
       </div>
       <div class="col-md-4 col-sm-6 portfolio-item">
@@ -89,16 +89,208 @@
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/06-thumbnail.jpg" alt="">
+          <img class="img-fluid" src="img/portfolio/act_productiva-thumbnail.jpg" alt="">
         </a>
         <div class="portfolio-caption">
-          <h4>Window</h4>
-          <p class="text-muted">Photography</p>
+          <h4>Personal</h4>
+          <p class="text-muted">Gestion de personal</p>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<!-- modales del portafolio-->
+
+    <!-- Modal 1 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Vizualizacion de planos</h2>
+                  <p class="item-intro text-muted">Pantalla para la vizualizacion del plano del campamento.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/gm.jpg" alt="">
+                  <p>Pantalla donde se visualizan los planos del campamento y el avance diario de los poligonos que se estan trabajando!</p>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Cerrar Imagen</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal 2 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Gestion de Equipo</h2>
+                  <p class="item-intro text-muted">Pantalla para la gestion de equipos pesados.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/act_equipo.jpg" alt="">
+                  <p>Pantalla donde se adiciona, modifica un equipo pesado de la empresa.</p>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Cerrar imagen</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal 3 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Gestion de actividad productiva</h2>
+                  <p class="item-intro text-muted">Pantalla para la gestion de actividades productivas.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/act_productiva.jpg" alt="">
+                  <p>Pantalla para la gestion de actividades productivas como adicion, modificacion...</p>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Cerrar imagen</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal 4 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Project Name</h2>
+                  <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/act_productiva.jpg" alt="">
+                  <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                  <ul class="list-inline">
+                    <li>Date: January 2017</li>
+                    <li>Client: Lines</li>
+                    <li>Category: Branding</li>
+                  </ul>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal 5 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Project Name</h2>
+                  <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/act_productiva.jpg" alt="">
+                  <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                  <ul class="list-inline">
+                    <li>Date: January 2017</li>
+                    <li>Client: Southwest</li>
+                    <li>Category: Website Design</li>
+                  </ul>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal 6 -->
+    <div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2 class="text-uppercase">Project Name</h2>
+                  <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/act_productiva.jpg" alt="">
+                  <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                  <ul class="list-inline">
+                    <li>Date: January 2017</li>
+                    <li>Client: Window</li>
+                    <li>Category: Photography</li>
+                  </ul>
+                  <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 <?php
   include_once("footer.php");
